3a9bb563e750b4a0d21bc2141e003e881b54ffba
deepakvictor <deepakvictor7585@gmail.com>
2021-08-07 19:49:51 +0530

    last

--- alw.py
+++ alw.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import time
 
 token = "ODU1MDkzOTEwMzAzNjcwMzAz.YMteQw.NymDbkb7M3PePzAt4r-BbGzK7r8"
 
@@ -7,6 +8,13 @@ bot = commands.Bot(command_prefix = '*')
 @bot.command()
 async def test(ctx):
     await ctx.send("i heard it my son! {}".format(ctx.author))
+
+@bot.command()
+async def c(ctx):
+    for i in range(1,61,10):
+        j = "The Wait over is : "+str(i) + " seconds"
+        await ctx.send(j,delete_after = 2)
+        time.sleep(9.7)
 
 
 @bot.command()
